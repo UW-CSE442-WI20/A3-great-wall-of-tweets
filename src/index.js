3b49252e548443848af5172397064700601256af
nancyli <nli57@cs.washington.edu>
2020-02-02 14:52:42 -0800

    Update scatterplot

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const d3 = require('d3');
 
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 60},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = 1300 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
@@ -19,20 +19,27 @@ var svg = d3.select("#my_dataviz")
         "translate(" + margin.left + "," + margin.top + ")");
 
 //Read the data
+var parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
 const csvFile = require('./cleaner_csv.csv');
 d3.csv(csvFile).then(function(data) {
-    console.log(data);
+    data.forEach(function(d) {
+        d.Date = parseTime(d.Date);
+    });
+
+    //console.log(data);
     // Add X axis
-    var x = d3.scaleLinear()
-        .domain([0, 100000])
-        .range([ 0, height]);
+    var x = d3.scaleTime().range([0, width]);
+    x.domain(d3.extent(data, function(d) { return d.Date; }));
+    //var x = d3.scaleLinear()
+    //    .domain([0, 100000])
+    //    .range([ 0, height]);
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
 
     // Add Y axis
     var y = d3.scaleLinear()
-        .domain([0, 100000])
+        .domain([0, 80000])
         .range([ height, 0]);
     svg.append("g")
         .call(d3.axisLeft(y));
@@ -46,7 +53,7 @@ d3.csv(csvFile).then(function(data) {
     // Highlight the specie that is hovered
     var highlight = function(d){
 
-        selected_specie = d.Likes
+        selected_specie = d.Date;
 
         d3.selectAll(".dot")
             .transition()
@@ -58,7 +65,7 @@ d3.csv(csvFile).then(function(data) {
             .transition()
             .duration(200)
             .style("fill", color(selected_specie))
-            .attr("r", 7)
+            .attr("r", 5)
     }
 
     // Highlight the specie that is hovered
@@ -67,7 +74,7 @@ d3.csv(csvFile).then(function(data) {
             .transition()
             .duration(200)
             .style("fill", "lightgrey")
-            .attr("r", 5 )
+            .attr("r", 3 )
     }
 
     // Add dots
@@ -76,10 +83,10 @@ d3.csv(csvFile).then(function(data) {
         .data(data)
         .enter()
         .append("circle")
-        .attr("class", function (d) { return "dot " + d.Likes } )
-        .attr("cx", function (d) { return x(d.Likes); } )
+        .attr("class", function (d) { return "dot " + d.Date } )
+        .attr("cx", function (d) { return x(d.Date); } )
         .attr("cy", function (d) { return y(d.Retweets); } )
-        .attr("r", 5)
+        .attr("r", 3)
         .style("fill", "blue" )
         .on("mouseover", highlight)
         .on("mouseleave", doNotHighlight )
