13f2a1ede9b9660fe4d82491ea2f2940f68692a7
nancyli <nli57@cs.washington.edu>
2020-02-02 16:37:38 -0800

    Update tooltip and zoom

--- src/index.js
+++ src/index.js
@@ -22,73 +22,96 @@ var svg = d3.select("#my_dataviz")
 var parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
 const csvFile = require('./cleaner_csv.csv');
 d3.csv(csvFile).then(function(data) {
+    // Convert to Date format
     data.forEach(function(d) {
         d.Date = parseTime(d.Date);
     });
 
-    //console.log(data);
     // Add X axis
-    var x = d3.scaleTime().range([0, width]);
-    x.domain(d3.extent(data, function(d) { return d.Date; }));
-    //var x = d3.scaleLinear()
-    //    .domain([0, 100000])
-    //    .range([ 0, height]);
-    svg.append("g")
+    var x = d3.scaleTime()
+        .domain(d3.extent(data, function(d) { return d.Date; }))
+        .range([0, width]);
+    var xAxis = svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
 
     // Add Y axis
     var y = d3.scaleLinear()
-        .domain([0, 80000])
+        .domain([0, 20])
         .range([ height, 0]);
-    svg.append("g")
+    var yAxis = svg.append("g")
         .call(d3.axisLeft(y));
 
-    // Color scale: give me a specie name, I return a color
-    var color = d3.scaleOrdinal()
-        .domain(["setosa", "versicolor", "virginica" ])
-        .range([ "#440154ff", "#21908dff", "#fde725ff"])
+    // Define the div for the tooltip
+    var div = d3.select("body")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
 
-
-    // Highlight the specie that is hovered
-    var highlight = function(d){
-
-        selected_specie = d.Date;
-
-        d3.selectAll(".dot")
-            .transition()
-            .duration(200)
-            .style("fill", "lightgrey")
-            .attr("r", 3)
-
-        d3.selectAll("." + selected_specie)
-            .transition()
-            .duration(200)
-            .style("fill", color(selected_specie))
-            .attr("r", 5)
-    }
-
-    // Highlight the specie that is hovered
-    var doNotHighlight = function(){
-        d3.selectAll(".dot")
-            .transition()
-            .duration(200)
-            .style("fill", "lightgrey")
-            .attr("r", 3 )
-    }
+    // Add a clipPath: everything out of this area won't be drawn.
+    var clip = svg.append("defs").append("svg:clipPath")
+        .attr("id", "clip")
+        .append("svg:rect")
+        .attr("width", width )
+        .attr("height", height )
+        .attr("x", 0)
+        .attr("y", 0);
 
     // Add dots
-    svg.append('g')
-        .selectAll("dot")
+    var scatter = svg.append('g')
+        .attr("clip-path", "url(#clip)");
+    scatter.selectAll("dot")
         .data(data)
         .enter()
         .append("circle")
         .attr("class", function (d) { return "dot " + d.Date } )
         .attr("cx", function (d) { return x(d.Date); } )
-        .attr("cy", function (d) { return y(d.Retweets); } )
+        .attr("cy", function (d) { return y(d.Popularity_log); } )
         .attr("r", 3)
         .style("fill", "blue" )
-        .on("mouseover", highlight)
-        .on("mouseleave", doNotHighlight )
+        .on("mouseover", function(d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", .9);
+            div.text(d.Tweet_Text)
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+        })
+        .on("mouseout", function(d) {
+            div.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
 
+    // Zoom feature
+    var zoom = d3.zoom()
+        .scaleExtent([0.5, 20])
+        .extent([[0, 0], [width, height]])
+        .on("zoom", updateChart);
+
+    svg.append("rect")
+        .attr("width", width)
+        .attr("height", height)
+        .style("fill", "none")
+        .style("pointer-events", "all")
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+        .call(zoom);
+
+    // Updates chart when zooming
+    function updateChart() {
+
+        // recover the new scale
+        var newX = d3.event.transform.rescaleX(x);
+        var newY = d3.event.transform.rescaleY(y);
+
+        // update axes with these new boundaries
+        xAxis.call(d3.axisBottom(newX))
+        yAxis.call(d3.axisLeft(newY))
+
+        // update circle position
+        scatter
+            .selectAll("circle")
+            .attr('cx', function(d) {return newX(d.Date)})
+            .attr('cy', function(d) {return newY(d.Popularity_log)});
+    }
 })
